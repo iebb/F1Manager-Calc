3167dcd00f9e363df5029e4ca040914768f6ad30
ieb^ <jebwizoscar@hotmail.com>
2023-04-18 15:42:24 +0800

    add catch

--- libs/storage/cloudStorage.js
+++ libs/storage/cloudStorage.js
@@ -6,17 +6,21 @@ export default function createWebStorage(type) {
       return new Promise((resolve, reject) => {
         axios.get(`/api/cloud/storage`).then(d => resolve(
           d.data ? d.data[key] : null
-        ))
+        )).catch(r => reject(r))
       })
     },
     setItem: (key, item) => {
       return new Promise((resolve, reject) => {
-        resolve(axios.post(`/api/cloud/storage`, {[key]: item}))
+        axios.post(`/api/cloud/storage`, {[key]: item}).then(
+          r => resolve(r)
+        ).catch(r => reject(r))
       })
     },
     removeItem: (key) => {
       return new Promise((resolve, reject) => {
-        resolve(axios.post(`/api/cloud/storage`, {[key]: null}))
+        axios.post(`/api/cloud/storage`, {[key]: null}).then(
+          r => resolve(r)
+        ).catch(r => reject(r))
       })
     },
   }
