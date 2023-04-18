19ec328c023e61abbb56bd31d25b4c25da8bfda0
ieb^ <jebwizoscar@hotmail.com>
2023-04-18 20:26:07 +0800

    cloud-config

--- components/LogIn.jsx
+++ components/LogIn.jsx
@@ -5,21 +5,14 @@ export default function LogIn() {
   const { data: session } = useSession()
   const [anchorElUser, setAnchorElUser] = useState(null);
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-
   if (session) {
     return (
       <div>
         <Chip label="cloud sync on" color="primary"/>
         <Tooltip title="Open settings">
-          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, ml: 3 }}>
+          <IconButton onClick={(event) => {
+            setAnchorElUser(event.currentTarget);
+          }} sx={{ p: 0, ml: 3 }}>
             <Avatar alt={session.user.name} src={session.user.image} />
           </IconButton>
         </Tooltip>
@@ -37,7 +30,9 @@ export default function LogIn() {
             horizontal: 'right',
           }}
           open={Boolean(anchorElUser)}
-          onClose={handleCloseUserMenu}
+          onClose={() => {
+            setAnchorElUser(null)
+          }}
         >
           <MenuItem>
             <Typography textAlign="center">Signed in as {session.user.name}#{session.user.discord_profile.discriminator}</Typography>
@@ -51,11 +46,16 @@ export default function LogIn() {
   }
   return (
     <div>
-      <Button
-        variant="contained"
-        sx={{ ml: 3 }}
-        onClick={() => signIn("discord")}
-      >Discord Sign in</Button>
+      <div>
+        <Button
+          variant="contained"
+          sx={{ ml: 3 }}
+          onClick={() => signIn("discord")}
+        >Discord Sign in</Button>
+      </div>
+      <div>
+        <Chip label="cloud sync disabled" color="default"/>
+      </div>
     </div>
   )
 }
