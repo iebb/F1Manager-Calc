9b62087baafca91a659cf60f0ca504bde3432c53
ieb <jebwizoscar@hotmail.com>
2023-05-17 15:28:04 +0800

    ads

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700&display=swap"
             rel="stylesheet"
           />
-          <script async
-                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3253159471656308"
-                  crossOrigin="anonymous"></script>
         </Head>
         <body>
+        <div dangerouslySetInnerHTML={{ __html: `<script async
+                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3253159471656308"
+                  crossOrigin="anonymous"></script>` }} />
         <div dangerouslySetInnerHTML={{ __html: `<script defer data-domain="f1setup.it" src="https://analytics.nekoko.it/js/script.js"></script>` }} />
         <Main />
         <NextScript />
