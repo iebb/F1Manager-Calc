0c7046e20210419b7d7ae84d57eb395470a6fdff
ieb <jebwizoscar@hotmail.com>
2023-05-17 15:06:45 +0800

    ads

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,15 @@
 import {Container, Divider, Typography} from "@mui/material";
+import GoogleAd from "./GoogleAd";
 
 export default function Footer() {
   return (
     <Container  maxWidth="xl" component="main" sx={{ pt: 2, pb: 3 }}>
+      <GoogleAd
+        style={{ display: 'block' }}
+        googleAdId="ca-pub-3253159471656308"
+        format="autorelaxed"
+        slot="1185564246"
+      />
       <Divider variant="fullWidth" />
       <Typography>
         Another ieb Project &middot; {' '}
