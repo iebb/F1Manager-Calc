b89f7ce51930a80e54e7a9492da497aa20d92e96
ieb^ <jebwizoscar@hotmail.com>
2023-09-04 02:16:32 +0800

    noreport

--- pages/api/report_vals.js
+++ pages/api/report_vals.js
@@ -2,17 +2,17 @@
 import {databasePromise} from "../../libs/cloud/mongodb";
 
 export const handler = async (req, res) => {
-  let db = await databasePromise;
-
-  const { uid, track, values } = req.body;
-  await db.collection('values').updateOne(
-    { track },
-    {
-      $inc: { [`feedback_cnt`]: 1 },
-      $push: { [`values`]: {uid, track, values} },
-    },
-    { upsert: true },
-  );
+  // let db = await databasePromise;
+  //
+  // const { uid, track, values } = req.body;
+  // await db.collection('values').updateOne(
+  //   { track },
+  //   {
+  //     $inc: { [`feedback_cnt`]: 1 },
+  //     $push: { [`values`]: {uid, track, values} },
+  //   },
+  //   { upsert: true },
+  // );
   res.status(200).json({status: "ok"});
 };
 export default handler;
